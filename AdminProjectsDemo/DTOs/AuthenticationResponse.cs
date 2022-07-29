64cf2b79e58989f18ca962b5646552354e421d58
Ebarrios <guayoswing@gmail.com>
2022-07-29 01:06:39 -0600

    Reset repository

--- AdminProjectsDemo/DTOs/AuthenticationResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AdminProjectsDemo.DTOs
-{
-    public class AuthenticationResponse
-    {
-        public string Token { get; set; }
-        public DateTime ExpirationDate { get; set; }
-    }
-}
